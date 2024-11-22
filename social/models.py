25a841fcc956ee007cf0e4518f98987ba3f6174f
Omar <omaracosta1228@gmail.com>
2024-11-22 00:13:59 -0500

    first commit

---
+++ social/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField(unique=True)  # Asegura que el correo sea único
+    password = models.CharField(max_length=128)  # Almacena contraseñas en formato seguro (encriptado en autenticación)
+    nombre = models.CharField(max_length=50, default="Sin nombre")
+    image = models.ImageField(default='batman.png')
+    banner = models.ImageField(upload_to='banners/', blank=True, null=True)  # Imagen del banner
+    educacion = models.TextField(blank=True, null=True)  # Información de educación
+    habilidades = models.TextField(blank=True, null=True)  # Habilidades
+    certificaciones = models.TextField(blank=True, null=True)  # Certificaciones
+
+    def __str__(self):
+        return f'{self.nombre} ({self.user.username})'
+
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    timestamp = models.DateTimeField(default=timezone.now)
+    content = models.TextField()  # Descripción del post
+    image = models.ImageField(upload_to='posts/', blank=True, null=True)  # Imagen opcional
+
+    class Meta:
+        ordering = ['-timestamp']
+
+    def __str__(self):
+        return f'{self.user.username}: {self.content[:20]}...'  # Muestra solo los primeros 20 caracteres
---
+++ social/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth.views import LoginView, LogoutView
+
+urlpatterns = [
+    path('', views.feed, name='feed'),
+    path('profile/', views.profile, name='profile'),
+    path('profile/<str:username>/', views.profile, name='profile'),
+    path('register/', views.register, name='register'),
+    path('login/', LoginView.as_view(template_name='social/login.html'), name='login'),
+	path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('post/', views.post, name='post'),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
